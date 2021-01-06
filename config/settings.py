0ecd478d84edad8a9d81d6fe18d5216ded965a8a
cmpack <67780115+cmpack@users.noreply.github.com>
2021-01-06 15:39:09 +0900

    Create seed_users&rooms

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ DJANGO_APPS = [
     "conversations.apps.ConversationsConfig",
 ]
 
-THIRD_PARTY_APPS = ["django_countries"]
+THIRD_PARTY_APPS = ["django_countries", "django_seed"]
 
 PROJECT_APPS = [
     "core.apps.CoreConfig",
