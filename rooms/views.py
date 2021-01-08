23656e145e08579c5fa5144a26a87dec219f4845
cmpack <67780115+cmpack@users.noreply.github.com>
2021-01-08 15:14:35 +0900

    Practice urls, views and httpresponse

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,18 @@
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def all_rooms(request):
+    now = datetime.now()
+    return HttpResponse(content=f"<h1>{now}</h1>")
+
+
+"""
 from django.shortcuts import render
 
-# Create your views here.
+
+def all_rooms(request):
+    return render(request, "all_rooms")
+
+"""
