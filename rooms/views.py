e2d624fd2629b3209a2d2ed782fe393e6a12aee5
cmpack <67780115+cmpack@users.noreply.github.com>
2021-01-10 15:46:38 +0900

    Practice and learn templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,18 +1,11 @@
-from datetime import datetime
 from django.shortcuts import render
-from django.http import HttpResponse
+from . import models
 
 
 def all_rooms(request):
-    now = datetime.now()
-    return HttpResponse(content=f"<h1>{now}</h1>")
-
-
-"""
-from django.shortcuts import render
-
-
-def all_rooms(request):
-    return render(request, "all_rooms")
-
-"""
+    all_rooms = models.Room.objects.all()[1:2]
+    return render(
+        request,
+        "rooms/home.html",
+        context={"rooms": all_rooms},
+    )
