0a295bc35fd383fe419233e6cc73220e5abb7356
cmpack <67780115+cmpack@users.noreply.github.com>
2021-01-18 15:39:10 +0900

    Finish User Login & Logout with django forms

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "users"
+
+urlpatterns = [
+    path("login", views.LoginVeiw.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
+]
