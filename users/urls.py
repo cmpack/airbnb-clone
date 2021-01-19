03a94f7f9dbda48782c9f473985f5ba337ca09fb
cmpack <67780115+cmpack@users.noreply.github.com>
2021-01-19 15:33:42 +0900

    Finish Login & Logout and Signup Form

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("login", views.LoginVeiw.as_view(), name="login"),
+    path("login", views.LoginView.as_view(), name="login"),
     path("logout", views.log_out, name="logout"),
+    path("sigup", views.SignUpView.as_view(), name="signup"),
 ]
